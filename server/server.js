741d051d3af4c75a5777bb48cd958c44a986ddd0
DiegoCamilli <camillidiego@yahoo.com>
2023-06-22 17:43:46 -0700

    clean code

--- server/server.js
+++ server/server.js
@@ -1,42 +1,40 @@
-const express = require('express');
-const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const express = require('express')
+const path = require('path')
+const { ApolloServer } = require('apollo-server-express')
+const { authMiddleware } = require('./utils/auth')
+const { typeDefs, resolvers } = require('./schemas')
+const db = require('./config/connection')
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001
 
 async function startApolloServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware, // Apply authMiddleware to the context
-  });
+  })
 
-  await server.start();
+  await server.start()
 
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app })
 }
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')))
 }
 
 // Comment out the existing routes middleware
-// app.use(routes);
+// app.use(routes)
 
 db.once('open', () => {
   startApolloServer().then(() => {
     app.listen(PORT, () =>
       console.log(`🌍 Now listening on localhost:${PORT}`)
-    );
-  });
-});
-
-Here are the main changes:
+    )
+  })
+})
