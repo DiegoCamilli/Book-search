5d79f851272f9a145b6e14c3b02489fc4dee37ce
DiegoCamilli <camillidiego@yahoo.com>
2023-06-22 17:40:45 -0700

    commit for files

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware, // Apply authMiddleware to the context
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +28,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Comment out the existing routes middleware
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  startApolloServer().then(() => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
+  });
 });
+
+Here are the main changes:
