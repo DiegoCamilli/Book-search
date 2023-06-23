b6b19bcf4716b52cab057e86f7e6ccdf78905e20
DiegoCamilli <camillidiego@yahoo.com>
2023-06-23 00:59:41 -0700

    update server js and start front end develop

--- server/server.js
+++ server/server.js
@@ -8,33 +8,35 @@ const db = require('./config/connection')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-async function startApolloServer() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  })
+// const dotenv = require("dotenv")
+// dotenv.config()
 
-  await server.start()
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-  server.applyMiddleware({ app })
-}
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// Comment out the existing routes middleware
-// app.use(routes)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-db.once('open', () => {
-  startApolloServer().then(() => {
-    app.listen(PORT, () =>
-      console.log(`🌍 Now listening on localhost:${PORT}`)
-    )
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
   })
-})
+  };
+startApolloServer(typeDefs, resolvers);
